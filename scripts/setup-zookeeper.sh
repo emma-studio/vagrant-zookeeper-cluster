afc1541267f8389c8c1885f0f0bb2d272aeea439
rao-f <emmafu0918@gmail.com>
2015-12-01 14:43:52 -0800

    Update setup-zookeeper.sh
    add lines of "mkdir [dataDir]" and put server id into [dataDir]/myid

--- scripts/setup-zookeeper.sh
+++ scripts/setup-zookeeper.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 source "/vagrant/scripts/common.sh"
 
+NODE=21
+
+while getopts s: option
+do
+	case "${option}"
+	in
+		s) NODE=${OPTARG};;
+	esac
+done
+
 function installLocalZookeeper {
 	echo "install zookeeper from local file"
 	FILE=/vagrant/resources/$ZOOKEEPER_ARCHIVE
@@ -18,6 +28,14 @@ function setupZookeeper {
 #	mkdir -p /var/spark/conf/
 #	cp -fRp /vagrant/resources/spark/slaves /var/spark/conf/
     cp -f /vagrant/resources/zookeeper/zoo.cfg /usr/local/zookeeper/conf
+    # The -p (i.e., parents) option creates the specified intermediate directories for a new directory if they do not already exist
+    # mkdir /tmp/zookeeper and put a "myid" file containing node number is a must for zookeeper.
+    # should test to see if this file and directory has been set up succsessfully and if node number has been written into this file
+    if [ ! -d /tmp/zookeeper ]; then
+	    echo "creating zookeeper data dir..."
+	    mkdir -p /tmp/zookeeper
+	    echo "${NODE}" >> /tmp/zookeeper/myid
+	fi
 }
 
 function setupEnvVars {
